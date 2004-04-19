f927aaae756682f1c3ebe61af1425a8d94c9e7ef
Ralph Giles <giles@xiph.org>
2004-04-19 05:47:45 +0000

    Restore error propagation blocks removed by the previous patch. Thanks to thomasvs for noticing this.
    svn path=/trunk/theora/; revision=6542

--- autogen.sh
+++ autogen.sh
@@ -102,13 +102,13 @@ fi
 echo "Generating configuration files for $package, please wait...."
 
 echo "  $ACLOCAL $ACLOCAL_FLAGS"
-$ACLOCAL $ACLOCAL_FLAGS
+$ACLOCAL $ACLOCAL_FLAGS || exit 1
 echo "  autoheader"
 autoheader || exit 1
 echo "  $LIBTOOLIZE --automake"
-$LIBTOOLIZE --automake
+$LIBTOOLIZE --automake || exit 1
 echo "  $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
-$AUTOMAKE --add-missing $AUTOMAKE_FLAGS 
+$AUTOMAKE --add-missing $AUTOMAKE_FLAGS || exit 1
 echo "  autoconf"
 autoconf || exit 1
 
