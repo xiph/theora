73325da752b2361b8b6790fc59335aba9e25705c
Ralph Giles <giles@xiph.org>
2006-04-26 16:59:54 +0000

    Propagate fixes for when AUTOMAKE_OPTIONS doesn't list a minimum version number.
    svn path=/trunk/theora/; revision=11288

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 # Run this to set up the build system: configure, makefiles, etc.
 # (based on the version in enlightenment's cvs)
 
-package="theora"
+package="vorbisdec"
 
 olddir=`pwd`
 srcdir=`dirname $0`
@@ -22,21 +22,23 @@ echo "checking for autoconf... "
 
 VERSIONGREP="sed -e s/.*[^0-9\.]\([0-9]\.[0-9]\).*/\1/"
 VERSIONMKINT="sed -e s/[^0-9]//"
-
-ACLOCAL_FLAGS="-I m4"
                                                                                 
 # do we need automake?
 if test -r Makefile.am; then
-  AM_NEEDED=`fgrep AUTOMAKE_OPTIONS Makefile.am | $VERSIONGREP`
+  AM_OPTIONS=`fgrep AUTOMAKE_OPTIONS Makefile.am`
+  AM_NEEDED=`echo $AM_OPTIONS | $VERSIONGREP`
+  if test x"$AM_NEEDED" = "x$AM_OPTIONS"; then
+    AM_NEEDED=""
+  fi
   if test -z $AM_NEEDED; then
     echo -n "checking for automake... "
     AUTOMAKE=automake
     ACLOCAL=aclocal
     if ($AUTOMAKE --version < /dev/null > /dev/null 2>&1); then
+      echo "yes"
+    else
       echo "no"
       AUTOMAKE=
-    else
-      echo "yes"
     fi
   else
     echo -n "checking for automake $AM_NEEDED or later... "
@@ -105,15 +107,14 @@ echo "Generating configuration files for $package, please wait...."
 
 echo "  $ACLOCAL $ACLOCAL_FLAGS"
 $ACLOCAL $ACLOCAL_FLAGS || exit 1
-echo "  autoheader"
-autoheader || exit 1
 echo "  $LIBTOOLIZE --automake"
 $LIBTOOLIZE --automake || exit 1
+echo "  autoheader"
+autoheader || exit 1
 echo "  $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
 $AUTOMAKE --add-missing $AUTOMAKE_FLAGS || exit 1
 echo "  autoconf"
 autoconf || exit 1
 
-cd 
 cd $olddir
 $srcdir/configure --enable-maintainer-mode "$@" && echo
