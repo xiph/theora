4396a8f79857811b306a24cfbe91929e60759d11
Tim Terriberry <tterribe@xiph.org>
2007-04-16 14:23:24 +0000

    Disable the loop filter until I get a chance to fix it.
    svn path=/trunk/theora/; revision=12878

--- lib/dec/x86/x86state.c
+++ lib/dec/x86/x86state.c
@@ -29,8 +29,8 @@ void oc_state_vtable_init_x86(oc_theora_state *_state){
     _state->opt_vtable.frag_recon_inter2=oc_frag_recon_inter2_mmx;
     _state->opt_vtable.state_frag_copy=oc_state_frag_copy_mmx;
     _state->opt_vtable.state_frag_recon=oc_state_frag_recon_mmx;
-    _state->opt_vtable.state_loop_filter_frag_rows=
-     oc_state_loop_filter_frag_rows_mmx;
+    /*_state->opt_vtable.state_loop_filter_frag_rows=
+     oc_state_loop_filter_frag_rows_mmx;*/
     _state->opt_vtable.restore_fpu=oc_restore_fpu_mmx;
   }
   else oc_state_vtable_init_c(_state);
