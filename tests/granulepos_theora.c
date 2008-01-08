bca7ac75a642344e945a4d6f0433d33713040069
Ralph Giles <giles@xiph.org>
2008-01-08 02:07:04 +0000

    Port the granulepos test to the new api. It currently fails.
    Test the old api from both the new and old libraries.
    
    svn path=/trunk/theora/; revision=14379

--- tests/granulepos_theora.c
+++ tests/granulepos_theora.c
@@ -16,7 +16,7 @@
  ********************************************************************/
 
 #include <stdlib.h>
-#include <theora/theoraenc.h>
+#include <theora/theora.h>
 #include <math.h>
 
 #include "tests.h"
@@ -31,70 +31,82 @@ static int ilog(unsigned int v){
 }
 
 static int
-granulepos_test_encode (int frequency)
+granulepos_test_encode (int frequency, int auto_p)
 {
-  th_info ti;
-  th_enc_ctx *te;
+  theora_info ti;
+  theora_state th;
   int result;
   int frame, tframe, keyframe, keydist;
   int shift;
   double rate, ttime;
-  th_ycbcr_buffer yuv;
+  yuv_buffer yuv;
   unsigned char *framedata;
   ogg_packet op;
   long long int last_granule = -1;
 
-  INFO ("+ Initializing th_info struct");
-  th_info_init (&ti);
+  INFO ("+ Initializing theora_info struct");
+  theora_info_init (&ti);
 
-  ti.frame_width = 32;
-  ti.frame_height = 32;
-  ti.pic_width = ti.frame_width;
-  ti.pic_height = ti.frame_height;
-  ti.pic_x = 0;
-  ti.pic_y = 0;
+  ti.width = 32;
+  ti.height = 32;
+  ti.frame_width = ti.width;
+  ti.frame_height = ti.frame_height;
+  ti.offset_x = 0;
+  ti.offset_y = 0;
   ti.fps_numerator = 16;
   ti.fps_denominator = 1;
   ti.aspect_numerator = 1;
   ti.aspect_denominator = 1;
-  ti.colorspace = TH_CS_UNSPECIFIED;
-  ti.pixel_fmt = TH_PF_420;
+  ti.colorspace = OC_CS_UNSPECIFIED;
+  ti.pixelformat = OC_PF_420;
+  ti.target_bitrate = 0;
   ti.quality = 16;
-  ti.keyframe_granule_shift=ilog(frequency);
 
-  INFO ("+ Allocating encoder context");
-  te = th_encode_alloc(&ti);
-  if (te == NULL) {
-    INFO ("+ Clearing th_info");
-    th_info_clear(&ti);
+  ti.dropframes_p = 0;
+  ti.quick_p = 1;
+
+  /* check variations of automatic or forced keyframe choice */
+  ti.keyframe_auto_p = auto_p;
+  /* check with variations of the maximum gap */
+  ti.keyframe_frequency = frequency;
+  ti.keyframe_frequency_force = frequency;
+
+  ti.keyframe_data_target_bitrate = ti.target_bitrate * 1.5;
+  ti.keyframe_auto_threshold = 80;
+  ti.keyframe_mindistance = MIN(8, frequency);
+  ti.noise_sensitivity = 1;
+
+  INFO ("+ Initializing theora_state for encoding");
+  result = theora_encode_init (&th, &ti);
+  if (result == OC_DISABLED) {
+    INFO ("+ Clearing theora_state");
+    theora_clear (&th);
+  } else if (result < 0) {
     FAIL ("negative return code initializing encoder");
   }
 
   INFO ("+ Setting up dummy 4:2:0 frame data");
-  framedata = calloc(ti.frame_height, ti.frame_width);
-  yuv[0].width = ti.frame_width;
-  yuv[0].height = ti.frame_height;
-  yuv[0].ystride = ti.frame_width;
-  yuv[0].data = framedata;
-  yuv[1].width = ti.frame_width / 2;
-  yuv[1].height = ti.frame_width / 2;
-  yuv[1].ystride = ti.frame_width;
-  yuv[1].data = framedata;
-  yuv[2].width = ti.frame_width / 2;
-  yuv[2].height = ti.frame_width / 2;
-  yuv[2].ystride = ti.frame_width;
-  yuv[2].data = framedata;
+  framedata = calloc(ti.height, ti.width);
+  yuv.y_width = ti.width;
+  yuv.y_height = ti.height;
+  yuv.y_stride = ti.width;
+  yuv.y = framedata;
+  yuv.uv_width = ti.width / 2;
+  yuv.uv_height = ti.width / 2;
+  yuv.uv_stride = ti.width;
+  yuv.u = framedata;
+  yuv.v = framedata;
 
   INFO ("+ Checking granulepos generation");
-  shift = ti.keyframe_granule_shift;
+  shift = theora_granule_shift(&ti);
   rate = (double)ti.fps_denominator/ti.fps_numerator;
   for (frame = 0; frame < frequency * 2 + 1; frame++) {
-    result = th_encode_ycbcr_in (te, &yuv);
+    result = theora_encode_YUVin (&th, &yuv);
     if (result < 0) {
-      printf("th_encode_ycbcr_in() returned %d\n", result);
+      printf("theora_encode_YUVin() returned %d\n", result);
       FAIL ("negative error code submitting frame for compression");
     }
-    th_encode_packetout (te, frame >= frequency * 2, &op);
+    theora_encode_packetout (&th, frame >= frequency * 2, &op);
     if ((long long int)op.granulepos < last_granule)
       FAIL ("encoder returned a decreasing granulepos value");
     last_granule = op.granulepos;
@@ -102,28 +114,28 @@ granulepos_test_encode (int frequency)
     keydist = op.granulepos - (keyframe << shift);
     if ((keyframe + keydist) != frame + 1)
       FAIL ("encoder granulepos does not map to the correct frame number");
-    tframe = th_granule_frame (te, op.granulepos);
+    tframe = theora_granule_frame (&th, op.granulepos);
     if (tframe != frame)
-      FAIL ("th_granule_frame returned incorrect results");
-    ttime = th_granule_time(te, op.granulepos);
+      FAIL ("theora_granule_frame returned incorrect results");
+    ttime = theora_granule_time(&th, op.granulepos);
     if (fabs(rate*(frame+1) - ttime) > 1.0e-6)
-      FAIL ("th_granule_time returned incorrect results");
+      FAIL ("theora_granule_time returned incorrect results");
 #if DEBUG
     printf("++ frame %d granulepos %lld %d:%d %d %.3lfs\n", 
 	frame, (long long int)op.granulepos, keyframe, keydist,
-	tframe, th_granule_time (te, op.granulepos));
+	tframe, theora_granule_time (&th, op.granulepos));
 #endif
   }
 
   /* clean up */
   INFO ("+ Freeing dummy frame data");
-  free(framedata);
+  free (framedata);
 
-  INFO ("+ Clearing th_info struct");
-  th_info_clear(&ti);
+  INFO ("+ Clearing theora_info struct");
+  theora_info_clear (&ti);
 
-  INFO ("+ Freeing encoder context");
-  th_encode_free(te);
+  INFO ("+ Clearing theora_state");
+  theora_clear (&th);
 
   return 0;
 }
@@ -131,12 +143,12 @@ granulepos_test_encode (int frequency)
 int main(int argc, char *argv[])
 {
 
-  granulepos_test_encode (1);
-  granulepos_test_encode (2);
-  granulepos_test_encode (3);
-  granulepos_test_encode (4);
-  granulepos_test_encode (8);
-  granulepos_test_encode (64);
+  granulepos_test_encode (1, 1);
+  granulepos_test_encode (2, 1);
+  granulepos_test_encode (3, 1);
+  granulepos_test_encode (4, 1);
+  granulepos_test_encode (8, 1);
+  granulepos_test_encode (64, 1);
 
   exit (0);
 }
