1d80901d0b12886660206fc8a1c60811476be828
Ralph Giles <giles@xiph.org>
2008-01-08 01:08:12 +0000

    Add copies of the noop and comment unit tests using the new style api. Test the old style api unit tests with both the new and old libraries.
    svn path=/trunk/theora/; revision=14378

--- tests/noop_theora.c
+++ tests/noop_theora.c
@@ -15,29 +15,48 @@
 
  ********************************************************************/
 
-#include <theora/theoraenc.h>
+#include <theora/theora.h>
 
 #include "tests.h"
 
 static int
 noop_test_encode ()
 {
-  th_info ti;
-  th_enc_ctx *te;
+  theora_info ti;
+  theora_state th;
 
   INFO ("+ Initializing theora_info struct");
-  th_info_init (&ti);
+  theora_info_init (&ti);
 
-  INFO ("+ Allocating encoder context");
-  te = th_encode_alloc(&ti);
-  if (te == NULL)
-    FAIL("td_encode_alloc returned a null pointer");
+  INFO ("+ Initializing theora_state for encoding");
+  if (theora_encode_init (&th, &ti) != OC_DISABLED) {
+    INFO ("+ Clearing theora_state");
+    theora_clear (&th);
+  }
 
   INFO ("+ Clearing theora_info struct");
-  th_info_clear (&ti);
+  theora_info_clear (&ti);
 
-  INFO ("+ Freeing encoder context");
-  th_encode_free(te);
+  return 0;
+}
+
+static int
+noop_test_decode ()
+{
+  theora_info ti;
+  theora_state th;
+
+  INFO ("+ Initializing theora_info struct");
+  theora_info_init (&ti);
+
+  INFO ("+ Initializing theora_state for decoding");
+  theora_decode_init (&th, &ti);
+
+  INFO ("+ Clearing theora_state");
+  theora_clear (&th);
+
+  INFO ("+ Clearing theora_info struct");
+  theora_info_clear (&ti);
 
   return 0;
 }
@@ -45,16 +64,18 @@ noop_test_encode ()
 static int
 noop_test_comments ()
 {
-  th_comment tc;
+  theora_comment tc;
 
-  th_comment_init (&tc);
-  th_comment_clear (&tc);
+  theora_comment_init (&tc);
+  theora_comment_clear (&tc);
 
   return 0;
 }
 
 int main(int argc, char *argv[])
 {
+  /*noop_test_decode ();*/
+
   noop_test_encode ();
 
   noop_test_comments ();
