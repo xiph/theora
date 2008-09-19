1dd2bd01239130c0fd14cb6a9c24bc5157ff4285
Ralph Giles <giles@xiph.org>
2008-09-19 19:37:54 +0000

    Remove extern references for OC_*_QUANT_MIN.
    These were exported for the use of the theora-exp encoder and are not 
    needed in trunk. Part of a patch from issue 1297.
    
    svn path=/trunk/theora/; revision=15322

--- lib/dec/quant.h
+++ lib/dec/quant.h
@@ -24,19 +24,8 @@ typedef ogg_uint16_t   oc_quant_table[64];
 typedef oc_quant_table oc_quant_tables[64];
 
 
-
 /*Maximum scaled quantizer value.*/
 #define OC_QUANT_MAX          (1024<<2)
-
-
-
-/*Minimum scaled DC coefficient frame quantizer value for intra and inter
-   modes.*/
-extern unsigned OC_DC_QUANT_MIN[2];
-/*Minimum scaled AC coefficient frame quantizer value for intra and inter
-   modes.*/
-extern unsigned OC_AC_QUANT_MIN[2];
-
 
 
 void oc_dequant_tables_init(oc_quant_table *_dequant[2][3],
