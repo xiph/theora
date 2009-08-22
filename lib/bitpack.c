d9716984330bc711fb9d02356e170d6864b52546
Ralph Giles <giles@xiph.org>
2009-08-22 18:14:02 +0000

    Update copyright year on the OggTheora source code.
    svn path=/trunk/theora/; revision=16503

--- lib/bitpack.c
+++ lib/bitpack.c
@@ -5,7 +5,7 @@
  * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
  * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
  *                                                                  *
- * THE OggTheora SOURCE CODE IS (C) COPYRIGHT 1994-2008             *
+ * THE OggTheora SOURCE CODE IS (C) COPYRIGHT 1994-2009             *
  * by the Xiph.Org Foundation and contributors http://www.xiph.org/ *
  *                                                                  *
  ********************************************************************
