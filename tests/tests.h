4a0148a2328c445664b7dbd39010aacfc58ac384
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-09-29 17:54:56 +0100

    cmake: allow tests to run without config.h

--- tests/tests.h
+++ tests/tests.h
@@ -15,7 +15,9 @@
 
  ********************************************************************/
 
-#include "config.h"
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
